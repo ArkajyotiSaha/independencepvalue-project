d9123722a9f31f057ddbe654faa4a076a6d3832a
Arkajyoti Saha <arkajyotisaha93@gmail.com>
2022-09-09 12:44:01 -0700

    Add files via upload

--- independencepvalue/R/selective_p_val.R
+++ independencepvalue/R/selective_p_val.R
@@ -92,22 +92,25 @@ selective_p_val <- function(S, n, CP, c, k, d0=5, mc_iter= 1000){
     if(p2 > 1){
       V_d <- as.matrix(PV_d$output[ , - c(1, 2)])
       Pi <- volesti::Vpolytope(V = V_d)
-      triang = try(geometry::delaunayn(Pi@V))
-      prod_res <- 1
-      for(i in 1:p2){
-        prod_res <- prod_res * gamma((n - i)/2)/(gamma((n - p2 -  i)/2) * gamma((p1 - i + 1)/2) * gamma((p2 - i + 1)/2))
+      triang = try(geometry::delaunayn(Pi@V), silent = TRUE)
+      if(!inherits(triang,'try-error')){
+        prod_res <- 1
+        for(i in 1:p2){
+          prod_res <- prod_res * gamma((n - i)/2)/(gamma((n - p2 -  i)/2) * gamma((p1 - i + 1)/2) * gamma((p2 - i + 1)/2))
+        }
+        alpha <- log(pi^(p2/2) * 2^p *  prod_res)
+        f_tot <- function(x) {
+          dt <- dCCA(n, p, p2, alpha, x)
+          return(c(dt, dt* (prod(1 - x^2) < test_stat)))
+        }
+        part_int <- function(i, Pi, triang, f_tot){
+          if(stats::var(round(Pi@V[triang[i,], ][,1], 5)) == 0) Pi@V[triang[i,], ][,1][length(Pi@V[triang[i,], ][,1])] <- Pi@V[triang[i,], ][,1][length(Pi@V[triang[i,], ][,1])]*(1 - 10^(-5))
+          return(SimplicialCubature::adaptIntegrateSimplex(f_tot, t(Pi@V[triang[i,], ]), fDim = 2)$integral)
+        }
+        par_I_tot_list <- future.apply::future_sapply(1:nrow(triang), part_int, Pi, triang, f_tot, future.seed=TRUE)
+        du <- sum(par_I_tot_list[2,])/sum(par_I_tot_list[1,])
+        if(sum(par_I_tot_list[1,]) == 0){du <- 0}
       }
-      alpha <- log(pi^(p2/2) * 2^p *  prod_res)
-      f_tot <- function(x) {
-        dt <- dCCA(n, p, p2, alpha, x)
-        return(c(dt, dt* (prod(1 - x^2) < test_stat)))
-      }
-      part_int <- function(i, Pi, triang, f_tot){
-        if(stats::var(round(Pi@V[triang[i,], ][,1], 5)) == 0) Pi@V[triang[i,], ][,1][length(Pi@V[triang[i,], ][,1])] <- Pi@V[triang[i,], ][,1][length(Pi@V[triang[i,], ][,1])]*(1 - 10^(-5))
-        return(SimplicialCubature::adaptIntegrateSimplex(f_tot, t(Pi@V[triang[i,], ]), fDim = 2)$integral)
-      }
-      par_I_tot_list <- future.apply::future_sapply(1:nrow(triang), part_int, Pi, triang, f_tot, future.seed=TRUE)
-      du <- sum(par_I_tot_list[2,])/sum(par_I_tot_list[1,])
     }
   }
   if(du <= 0 || du >= 1 || p2 > d0){
