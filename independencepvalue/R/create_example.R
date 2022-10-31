cba02b92367182bae8bca08f9da56f22001f3528
jacobbien <jbien@usc.edu>
2022-10-31 14:54:22 -0700

    very minor changes

--- independencepvalue/R/create_example.R
+++ independencepvalue/R/create_example.R
@@ -1,6 +1,7 @@
 # Generated from create-independencepvalue.Rmd: do not edit by hand
 
 #' Function to create a population correlation matrix for the vignette
+#' 
 #' This function creates a `p` x `p` matrix where the diagonal blocks are two p/2 equicorrelation matrix with parameter `a`. The all the elements in the offdiagonal blocks of the matrix are equal to `b`. `p` has to be an even number. 
 #' 
 #' @param p total number of variables, must be even
